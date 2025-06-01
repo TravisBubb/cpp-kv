8247f0217648f2cb522417b8735010822bd4c7b7
Travis Bubb <travisbubb14@gmail.com>
2025-06-01 10:50:09 -0400

    feat: Handle SIGINT and SIGTERM signals to cleanly shutdown the storage node server

--- apps/storage-node/main.cpp
+++ apps/storage-node/main.cpp
@@ -1,6 +1,18 @@
 #include "../../libs/common/include/logging.h"
 #include "grpcpp/server_builder.h"
 #include "in_memory_storage_service.h"
+#include <csignal>
+
+std::unique_ptr<grpc::Server> server;
+std::atomic<bool> shutdown_requested(false);
+
+void HandleShutdownSignal(int signum) {
+  LOG_INFO("Signal {} received. Shutting down server...", signum);
+  shutdown_requested = true;
+  if (server) {
+    server->Shutdown();
+  }
+}
 
 void RunServer() {
   std::string server_addr("0.0.0.0:50051");
@@ -10,14 +22,31 @@ void RunServer() {
   builder.AddListeningPort(server_addr, grpc::InsecureServerCredentials());
   builder.RegisterService(&service);
 
-  std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+  server = builder.BuildAndStart();
   LOG_INFO("Server listening on {}", server_addr);
-
   server->Wait();
+  LOG_INFO("Server stopped");
 }
 
 int main() {
   logging::init();
-  RunServer();
+
+  // Setup signal handler
+  std::signal(SIGINT, HandleShutdownSignal);
+  std::signal(SIGTERM, HandleShutdownSignal);
+
+  std::thread server_thread(RunServer);
+
+  while (!shutdown_requested) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  // Wait for server thread to finish after shutdown
+  if (server_thread.joinable()) {
+    server_thread.join();
+  }
+
+  LOG_INFO("Server shut down complete");
+
   return 0;
 }

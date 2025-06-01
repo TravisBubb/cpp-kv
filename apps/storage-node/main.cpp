dd5b5f9173404ed901d46d7cbb7b56a5ebd327c2
Travis Bubb <travisbubb14@gmail.com>
2025-06-01 10:40:59 -0400

    feat: Add logging infrastructure using spdlog

--- apps/storage-node/main.cpp
+++ apps/storage-node/main.cpp
@@ -1,7 +1,6 @@
+#include "../../libs/common/include/logging.h"
 #include "grpcpp/server_builder.h"
-#include "in_memory_engine.h"
 #include "in_memory_storage_service.h"
-#include <iostream>
 
 void RunServer() {
   std::string server_addr("0.0.0.0:50051");
@@ -12,48 +11,13 @@ void RunServer() {
   builder.RegisterService(&service);
 
   std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-  std::cout << "[DEBUG] Server listening on " << server_addr << std::endl;
+  LOG_INFO("Server listening on {}", server_addr);
 
   server->Wait();
 }
 
 int main() {
-  {
-    auto engine = InMemoryStorageEngine();
-    std::string v1 = "{ \"name\": \"travis\" }";
-    std::printf("Setting k1:v1\n");
-    engine.set("k1", std::vector<uint8_t>(v1.begin(), v1.end()));
-    StorageResult res = engine.get("k1");
-    if (res.get_status() == StorageStatus::OK) {
-      if (!res.get_data().has_value()) {
-        std::printf("No value found for k1\n");
-        return 1;
-      }
-      std::vector<std::uint8_t> bytes = res.get_data().value();
-      std::string str = std::string(bytes.begin(), bytes.end());
-      std::printf("Retrieved k1:%s\n", str.c_str());
-    } else {
-      std::printf("Received error status\n");
-    }
-  }
-
-  auto hydrated_engine = InMemoryStorageEngine();
-  hydrated_engine.hydrate();
-
-  StorageResult res = hydrated_engine.get("k1");
-  if (res.get_status() == StorageStatus::OK) {
-    if (!res.get_data().has_value()) {
-      std::printf("No value found for k1\n");
-      return 1;
-    }
-    std::vector<std::uint8_t> bytes = res.get_data().value();
-    std::string str = std::string(bytes.begin(), bytes.end());
-    std::printf("Retrieved k1:%s\n", str.c_str());
-  } else {
-    std::printf("Received error status\n");
-  }
-
+  logging::init();
   RunServer();
-
   return 0;
 }
